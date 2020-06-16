3a55f68d1c32e9e2a2ff6e3f69d12463493d41f5
tarwin3v <tarwin.ev@gmail.com>
2020-06-16 19:12:47 +0200

    edgeInsets & image.network

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' show get;
 import 'models/image_model.dart';
+import 'widgets/image_list.dart';
 import 'dart:convert';
 
 class App extends StatefulWidget {
@@ -13,17 +14,22 @@ class App extends StatefulWidget {
 
 class AppState extends State<App> {
   int counter = 0;
+  List<ImageModel> images = [];
 
   void fetchImage() async {
     counter ++;
     var res = await get('https://jsonplaceholder.typicode.com/photos/$counter');
     var imageInstance = ImageModel.fromJson(json.decode(res.body));
+
+    setState(() {
+      images.add(imageInstance);
+    });
   }
 
   Widget build(ctx){
    return MaterialApp(
     home: Scaffold(
-      body: Text('$counter'),
+      body: ImageList(images),
       appBar: AppBar(
         title: Text('Lets see some images'),
       ),
---
+++ lib/src/widgets/image_list.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import '../models/image_model.dart';
+
+
+
+class ImageList extends StatelessWidget {
+  final List<ImageModel> images;
+
+  ImageList(this.images);
+
+  Widget build(ctx) {
+    return ListView.builder(
+      itemCount: images.length,
+      itemBuilder: (context,int index){
+        return Container(
+          margin: EdgeInsets.all(20.0),
+          padding: EdgeInsets.all(20.0),
+          child: Image.network(images[index].url),
+          
+        );
+      },
+    );
+  }
+  
+}
